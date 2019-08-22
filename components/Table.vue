7364bdbc3a6dc827b2a270782441fe5ba63a41fe
Evandro Nakayama Mota <evandro.nmota5@gmail.com>
2019-08-22 14:00:14 -0300

    Add Group By

--- components/Table.vue
+++ components/Table.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-form>
       <v-text-field v-model="search" append-icon="search" label="Search"></v-text-field>
+      <v-select :items="items" label="Unidade" v-model="select"></v-select>
     </v-form>
     <v-data-table :headers="headers" :items="sheet" :search="search"></v-data-table>
   </v-app>
@@ -13,12 +14,19 @@ export default {
   data: () => ({
     sheet: [],
     headers: [],
-    search: ""
+    search: "",
+    select: "",
+    items: ["IME", "FAU", "FEA", "EP"]
   }),
   methods: {
     set() {
       this.sheet = this.s;
       this.headers = this.h;
+      this.headers.push({
+        filter: (value, search, item) => {
+          return !this.select ? true : item.unity == this.select;
+        }
+      });
     }
   },
   created() {
