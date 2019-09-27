bcbffef0ba8b768c53cd6678b87d5c4027ac0679
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2019-09-26 21:45:04 -0300

    Init home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,27 @@
 <template>
-  <v-app>HOME</v-app>
+  <v-app class="transparent">
+    <v-container fluid fill-height align-center>
+      <v-row no-gutters>
+        <v-col>
+          <v-row class="display-4">SOLUS</v-row>
+          <v-row
+            class="display-1"
+          >Onde você encontra as iniciativas de inovação e empreendedorismo nos diversos campi da USP.</v-row>
+        </v-col>
+        <v-col>
+          <v-img src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-container>INFO</v-container>
+  </v-app>
 </template>
+
+<script>
+</script>
+
+<style scoped>
+.transparent {
+  background-color: transparent;
+}
+</style>
