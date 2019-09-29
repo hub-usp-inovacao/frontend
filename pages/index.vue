d1a30b8201b855dfe39d66215436163dee67ca41
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2019-09-29 18:24:58 -0300

    Try layout of Home

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,63 @@
 <template>
-  <v-app class="transparent">
-    <v-container fluid fill-height align-center>
-      <v-row no-gutters>
+  <v-app>
+    <v-container class="d-block">
+      <v-row>
+        <v-col class="align-self-end">
+          <p class="display-4 all">SOLUS</p>
+          <p
+            class="headline all"
+          >Onde você encontra as iniciativas de inovação e empreendedorismo nos diversos campi da USP.</p>
+        </v-col>
         <v-col>
-          <v-row class="display-4">SOLUS</v-row>
-          <v-row
-            class="display-1"
-          >Onde você encontra as iniciativas de inovação e empreendedorismo nos diversos campi da USP.</v-row>
+          <v-img src="https://picsum.photos/400/250?random"></v-img>
         </v-col>
       </v-row>
     </v-container>
-    <v-container>INFO</v-container>
+
+    <v-container>
+      <template>
+        <v-timeline>
+          <v-timeline-item v-for="item in options" :key="item.title" hide-dot>
+            <v-img slot="opposite" src="https://picsum.photos/300/200?random"></v-img>
+
+            <v-container>
+              <div class="headline">{{item.title}}</div>
+              <div>{{item.description}}</div>
+            </v-container>
+          </v-timeline-item>
+        </v-timeline>
+      </template>
+    </v-container>
   </v-app>
 </template>
 
 <script>
+export default {
+  data: () => ({
+    options: [
+      {
+        title: "Iniciativas",
+        description:
+          "Organizações e programas para o estímulo e suporte da inovação e empreendedorismo na Universidade."
+      },
+      {
+        title: "Desenvolvimento & Inovação",
+        description:
+          "Laboratórios e grupos de pesquisa que atuam em projetos de inovação e empreendedorismo."
+      },
+      {
+        title: "Educação",
+        description:
+          "Cursos e disciplinas para ensino de inovação e empreendedorismo na Universidade de São Paulo."
+      },
+      {
+        title: "Empresas",
+        description:
+          "Diretório de empresas criadas por alunos e ex-alunos USP ao longo dos anos."
+      }
+    ]
+  })
+};
 </script>
 
 <style scoped>
