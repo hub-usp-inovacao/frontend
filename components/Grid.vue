c65b6a04e6a6df68ffe0f24a1c8995bcbefb5de3
Evandro Nakayama Mota <evandro.nmota5@gmail.com>
2019-10-06 19:14:55 -0300

    Design

--- components/Grid.vue
+++ components/Grid.vue
@@ -11,6 +11,75 @@
       ></v-text-field>
 
       <div v-if="this.$vuetify.breakpoint.name == 'xs'">
+        <v-card class="mx-auto overflow-hidden" height="400" width="344">
+          <v-system-bar color="deep-purple darken-3"></v-system-bar>
+
+          <v-app-bar color="deep-purple accent-4" dark prominent>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+            <v-toolbar-title>My files</v-toolbar-title>
+
+            <div class="flex-grow-1"></div>
+
+            <v-btn icon>
+              <v-icon>mdi-magnify</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-filter</v-icon>
+            </v-btn>
+
+            <v-btn icon>
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </v-app-bar>
+
+          <v-navigation-drawer v-model="drawer" absolute bottom temporary>
+            <v-list nav dense>
+              <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+                <v-list-item>
+                  <v-list-item-title>Foo</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-title>Bar</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-title>Fizz</v-list-item-title>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-title>Buzz</v-list-item-title>
+                </v-list-item>
+              </v-list-item-group>
+            </v-list>
+          </v-navigation-drawer>
+
+          <v-card-text>The navigation drawer will appear from the bottom on smaller size screens.</v-card-text>
+        </v-card>
+        <v-bottom-navigation v-model="bottomNav" fixed>
+          <v-btn value="recent">
+            <span>Iniciativas</span>
+            <v-icon>mdi-history</v-icon>
+          </v-btn>
+
+          <v-btn value="favorites">
+            <span>D&I</span>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn value="nearby">
+            <span>Educação</span>
+            <v-icon>mdi-map-marker</v-icon>
+          </v-btn>
+
+          <v-btn value="nearby">
+            <span>Empresas</span>
+            <v-icon>mdi-map-marker</v-icon>
+          </v-btn>
+        </v-bottom-navigation>
+
         <v-select
           class="my-4"
           :items="propsIncubator"
@@ -110,6 +179,7 @@
             </v-container>
           </div>
         </masonry>
+
         <v-pagination
           v-model="page"
           :length="numberOfPages(props.pagination.itemsLength)"
@@ -136,7 +206,9 @@ export default {
     selectIncubator: "",
     selectCampus: "",
     selectUnity: "",
-    options: {}
+    options: {},
+    drawer: false,
+    group: null
   }),
   methods: {
     setCols() {
@@ -195,6 +267,9 @@ export default {
     }, 400),
     page() {
       this.$vuetify.goTo(this.$refs.start, this.options);
+    },
+    group() {
+      this.drawer = false;
     }
   },
   mounted() {
