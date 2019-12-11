81027fd729bd7085fceb307dd96c7f99893a7dcb
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2019-12-11 10:37:36 -0300

    Add overflow and min-height to expansion panels

--- pages/playground.vue
+++ pages/playground.vue
@@ -4,25 +4,26 @@
       <v-row>
         <v-col align-self="center">
           <v-container px-10>
-            <p class="display-2 white--text">Iniciativas</p>
+            <p class="display-2 white--text">Desenvolvimento e Inovação</p>
             <p class="title white--text font-weight-light">
-              Organizações e programas para o
-              estímulo e suporte da inovação e
-              empreendedorismo.
+              Laboratórios, organizações e
+              programas que atuam com
+              desenvolvimento & inovação.
             </p>
-            <v-btn depressed dark color="#da1955">Cadastre a sua</v-btn>
+            <v-btn depressed dark color="#da1955">Cadastre-se</v-btn>
           </v-container>
         </v-col>
         <v-col class="image-background" align-self="stretch"></v-col>
       </v-row>
     </v-app>
-    <v-container>
+
+    <v-container style="min-height: 100vh;">
       <v-row>
         <v-col>
-          <v-expansion-panels>
+          <v-expansion-panels v-model="panel">
             <v-expansion-panel v-for="items in this.tabs" :key="items.name">
               <v-expansion-panel-header class="title font-weight-regular">{{items.name}}</v-expansion-panel-header>
-              <v-expansion-panel-content>
+              <v-expansion-panel-content class="overflow">
                 <v-list-item-group>
                   <v-list-item
                     v-for="item in items.content"
@@ -35,10 +36,13 @@
           </v-expansion-panels>
         </v-col>
         <v-col>
-          <v-card style="position: sticky">
+          <v-card>
             <v-container v-if="showing">
               <v-card-title align="center" class="title">{{showing.name}}</v-card-title>
               <v-container class="font-weight-light">{{showing.description}}</v-container>
+              <div align="center">
+                <v-btn depressed dark color="#da1955" :href="showing.url">Visite o site</v-btn>
+              </div>
             </v-container>
           </v-card>
         </v-col>
@@ -50,6 +54,7 @@
 <script>
 export default {
   data: () => ({
+    panel: 0,
     showing: null,
     tabs: {
       tab1: {
@@ -113,6 +118,11 @@ export default {
 .image-background {
   background-image: url("http://imagens.usp.br/wp-content/uploads/Instala%C3%A7%C3%B5es-Instituto-Pasteur-USP_Foto-Marcos-Santos_U0Y8339.jpg");
   background-size: cover;
+}
+.overflow {
+  max-height: 50vh;
+  position: relative;
+  overflow-y: auto;
 }
 </style>>
 
