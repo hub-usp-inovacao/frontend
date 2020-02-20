92a7935ab9b7528409a405ae2b246b46adec0c51
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2020-02-20 20:45:52 -0300

    Fix overflow in home page

--- pages/index.vue
+++ pages/index.vue
@@ -5,9 +5,15 @@
         <v-carousel-item v-for="(photo,i) in photos" :key="i" :src="photo"></v-carousel-item>
       </v-carousel>
 
-      <div style="position: absolute; top: 0; height: 100%; width: 100%;" class="fit-page">
-        <v-row class="fill-height fit-page">
-          <v-col style="background-color: rgba(255, 167, 38, 0.9); " align-self="stretch">
+      <div style="position: absolute; top: 0; height: 100%; width: 100%;">
+        <v-row class="fill-height fit-page mx-0">
+          <v-col
+            style="background-color: rgba(255, 167, 38, 0.9); "
+            cols="12"
+            sm="8"
+            md="6"
+            align-self="stretch"
+          >
             <v-row class="fill-height" justify="center">
               <v-col align-self="center" cols="10" sm="8">
                 <v-container>
@@ -23,13 +29,12 @@
                     color="white"
                     label="Buscar no Solus"
                     append-outer-icon="search"
-                    :style="this.$vuetify.breakpoint.xs ? 'width: 100%' : 'width: 70%'"
+                    :style="this.$vuetify.breakpoint.xs ? 'width: 100%' : 'width: 90%'"
                   ></v-text-field>
                 </v-container>
               </v-col>
             </v-row>
           </v-col>
-          <v-spacer class="hidden-sm-and-down" />
         </v-row>
       </div>
     </v-app>
@@ -57,7 +62,7 @@
               </v-row>
 
               <v-row align="center">
-                <v-col>
+                <v-col cols="12" sm="6">
                   <p class="headline font-weight-medium">Um mundo de inovações.</p>
                   <p
                     class="title font-weight-light"
@@ -67,7 +72,7 @@
                 <v-col>
                   <img
                     :src="require('@/vectors/home_globo.svg')"
-                    style="max-height: 80vh; max-width: 100vw; object-fit: contain;"
+                    style="max-height: 80vh; max-width: 100%; object-fit: contain;"
                   />
                 </v-col>
               </v-row>
@@ -94,13 +99,14 @@
           </v-col>
         </v-row>
       </v-container>
+      <v-img :src="require('@/images/background_cards.svg')" width="100vw"></v-img>
     </v-app>
 
     <v-app>
       <v-container fill-height fluid style="background-color: rgba(255, 167, 38, 0.9); ">
         <v-container>
           <v-row justify="center" align="baseline">
-            <v-col cols="10" sm="8">
+            <v-col cols="10" sm="6">
               <p
                 class="headline white--text font-weight-medium text-center"
               >Abrangendo todos os campi da USP.</p>
@@ -109,6 +115,7 @@
                 class="title white--text font-weight-light text-center"
               >De Ribeirão Preto a Lorena. O Solus tem cobertura em todos os projetos da Universidade</p>
             </v-col>
+
             <img
               :src="require('@/vectors/home_mapa.svg')"
               style="max-width: 80vw; max-height: 80vh;"
