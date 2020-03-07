88ea57ab66e89333525ee459027ac7c5ebfc2ac3
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2020-03-07 12:30:45 -0300

    Add AUSPIN section to Home

--- pages/index.vue
+++ pages/index.vue
@@ -55,7 +55,10 @@
       <v-container fill-height>
         <v-row class="ma-0" align="center">
           <v-col cols="12" sm="7">
-            <p class="display-1 font-weight-medium ma-0">Um lugar só com:</p>
+            <p
+              :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'"
+              class="font-weight-medium"
+            >Um lugar só com:</p>
 
             <v-row
               class="fill-height"
@@ -64,29 +67,36 @@
             >
               <v-col cols="12">
                 <p
-                  class="title font-weight-light"
+                  :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                  class="font-weight-light"
                 >&bull; As diversas iniciativas e estruturas da USP.</p>
               </v-col>
 
               <v-col cols="12">
-                <p class="title font-weight-light">&bull; Os pesquisadores e suas competências.</p>
+                <p
+                  :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                  class="font-weight-light"
+                >&bull; Os pesquisadores e suas competências.</p>
               </v-col>
 
               <v-col cols="12">
                 <p
-                  class="title font-weight-light"
+                  :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                  class="font-weight-light"
                 >&bull; Todas as patentes que a USP detém e disponibiliza para parceria com empresas e outras instituições interessadas.</p>
               </v-col>
 
               <v-col cols="12">
                 <p
-                  class="title font-weight-light"
+                  :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                  class="font-weight-light"
                 >&bull; As disciplinas de graduação e pós-graduação sobre empreendedorismo e inovação.</p>
               </v-col>
 
               <v-col cols="12">
                 <p
-                  class="title font-weight-light"
+                  :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                  class="font-weight-light"
                 >&bull; As empresas com DNA USP, criadas a partir de iniciativas incubadas na USP ou que seus fundadores e criadores foram alunos ou pesquisadores da USP.</p>
               </v-col>
             </v-row>
@@ -107,46 +117,78 @@
       <v-container fill-height>
         <v-container>
           <v-container>
-            <p class="display-1 font-weight-medium">USP: Um lugar para empreender e inovar</p>
+            <p
+              :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'"
+              class="font-weight-medium"
+            >USP: Um lugar para empreender e inovar</p>
           </v-container>
 
           <v-container>
             <p
-              class="title font-weight-light"
+              :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+              class="font-weight-light"
             >A Universidade de São Paulo é, de acordo com diversos rankings nacionais e internacionais, a melhor universidade latino-americana e uma das melhores universidades do mundo. São mais de 5.000 professores e 70.000 alunos de graduação e pós, cuja significativa produção intelectual, está disponível para empresas e organizações por meio do:</p>
           </v-container>
 
-          <v-row justify="center" align="center" class="ma-0">
-            <v-col cols="6" lg="3">
+          <v-row justify="space-around" align="center" class="ma-0">
+            <v-col cols="5" lg="3">
               <v-row justify="center">
-                <img :src="require('@/vectors/home_patents_icon.svg')" />
+                <img
+                  :src="require('@/vectors/home_patents_icon.svg')"
+                  style="max-width: 100%; max-height: 100%;"
+                />
               </v-row>
 
-              <p class="mt-5 display-1 text-center" style="color: #4AD5FF">Licenciamento de patentes</p>
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'display-1'"
+                class="mt-5 text-center"
+                style="color: #4AD5FF"
+              >Licenciamento de patentes</p>
             </v-col>
 
-            <v-col cols="6" lg="3">
+            <v-col cols="5" lg="3">
               <v-row justify="center">
-                <img :src="require('@/vectors/home_partnership_icon.svg')" />
+                <img
+                  :src="require('@/vectors/home_partnership_icon.svg')"
+                  style="max-width: 100%; max-height: 100%;"
+                />
               </v-row>
 
-              <p class="mt-5 display-1 text-center" style="color: #4AD5FF">Parcerias de pesquisas</p>
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'display-1'"
+                class="mt-5 text-center"
+                style="color: #4AD5FF"
+              >Parcerias de pesquisas</p>
             </v-col>
 
-            <v-col cols="6" lg="3">
+            <v-col cols="5" lg="3">
               <v-row justify="center">
-                <img :src="require('@/vectors/home_teaching_icon.svg')" />
+                <img
+                  :src="require('@/vectors/home_teaching_icon.svg')"
+                  style="max-width: 100%; max-height: 100%;"
+                />
               </v-row>
 
-              <p class="mt-5 display-1 text-center" style="color: #4AD5FF">Cursos</p>
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'display-1'"
+                class="mt-5 text-center"
+                style="color: #4AD5FF"
+              >Cursos</p>
             </v-col>
 
-            <v-col cols="6" lg="3">
+            <v-col cols="5" lg="3">
               <v-row justify="center">
-                <img :src="require('@/vectors/home_events_icon.svg')" />
+                <img
+                  :src="require('@/vectors/home_events_icon.svg')"
+                  style="max-width: 100%; max-height: 100%;"
+                />
               </v-row>
 
-              <p class="mt-5 display-1 text-center" style="color: #4AD5FF">Eventos</p>
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'display-1'"
+                class="mt-5 text-center"
+                style="color: #4AD5FF"
+              >Eventos</p>
             </v-col>
           </v-row>
         </v-container>
@@ -160,10 +202,14 @@
           <v-row class="ma-0 fill-height" align="center">
             <v-col cols="12" sm="7">
               <p
-                class="display-1 white--text font-weight-medium mb-8"
+                :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'"
+                class="font-weight-medium white--text mb-8"
               >Solus: Encontre o que você procura</p>
 
-              <p class="title white--text font-weight-light">
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                class="white--text font-weight-light"
+              >
                 O Portal Solus facilita a conexão entre a sociedade e a USP.
                 Simples e intuitivo, basta digitar palavras-chave na ferramenta de
                 busca para obter os contatos de instituições, estruturas, empresas
@@ -195,7 +241,7 @@
     <v-app>
       <v-container fill-height>
         <v-container>
-          <v-row justify="center" class="ma-0">
+          <v-row justify="center" align-content="space-around" class="mx-0">
             <v-col cols="12" md="5">
               <v-row justify="center">
                 <img
@@ -206,13 +252,20 @@
             </v-col>
 
             <v-col cols="12" md="7">
-              <p class="display-1 font-weight-medium ma-0">Empresas com DNA USP:</p>
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'"
+                class="font-weight-medium mb-0"
+              >Empresas com DNA USP:</p>
 
               <p
-                class="display-1 font-weight-medium"
+                :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'"
+                class="font-weight-medium mb-8"
               >Resultados de uma Universidade Inovadora e Empreendora</p>
 
-              <p class="title font-weight-light">
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                class="font-weight-light"
+              >
                 A marca DNA USP foi criada para identificar empresas constituídas por
                 alunos, ex-alunos e pesquisadores que passaram pela Universidade de
                 São Paulo ao longo dos anos, bem como empresas resultantes de
@@ -221,8 +274,73 @@
               </p>
             </v-col>
           </v-row>
+
+          <v-row justify="center" class="mx-0">
+            <v-col cols="4" sm="3">
+              <v-row justify="center" align="center" class="fill-height">
+                <img
+                  :src="require('@/vectors/logo_gympass.svg')"
+                  style="max-width: 100%; max-height: 100%; object-fit: contain;"
+                />
+              </v-row>
+            </v-col>
+
+            <v-col cols="4" sm="3">
+              <v-row justify="center" align="center" class="fill-height">
+                <img
+                  :src="require('@/vectors/logo_ifood.svg')"
+                  style="max-width: 100%; max-height: 100%; object-fit: contain;"
+                />
+              </v-row>
+            </v-col>
+
+            <v-col cols="4" sm="3">
+              <v-row justify="center" align="center" class="fill-height">
+                <img
+                  :src="require('@/vectors/logo_99.svg')"
+                  style="max-width: 100%; max-height: 100%; object-fit: contain;"
+                />
+              </v-row>
+            </v-col>
+          </v-row>
         </v-container>
       </v-container>
+    </v-app>
+
+    <!-- AUSPIN -->
+    <v-app>
+      <v-container fill-height>
+        <v-container>
+          <v-row class="ma-0 fill-height" align="center">
+            <v-col cols="12" md="7">
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'"
+                class="font-weight-medium mb-8"
+              >AUSPIN: A Agência de Inovação e Empreendedorismo da USP</p>
+
+              <p
+                :class="$vuetify.breakpoint.smAndDown ? 'body-1' : 'title'"
+                class="font-weight-light"
+              >É responsável pelo registro e licenciamento de propriedade intelectual, pela transferência de tecnologias desenvolvidas na USP para empresas e organizações, e pelo fomento ao empreendedorismo junto a todas as unidades - faculdades, institutos, laboratórios - da USP. Com escritórios e representações que atendem todos os campi da USP, a AUSPIN está à sua disposição para facilitar seu contato com pesquisadores, laboratórios, empresas e estruturas relacionadas à inovação e ao empreendedorismo na Universidade de São Paulo.</p>
+            </v-col>
+
+            <v-col cols="12" md="5">
+              <v-row justify="center">
+                <img
+                  :src="require('@/vectors/logo_auspin.svg')"
+                  style="max-width: 100%; max-height: 100%; object-fit: contain;"
+                />
+              </v-row>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-container>
+    </v-app>
+
+    <!-- Conecte-se -->
+    <v-app>
+      <div class="light-blue-layer"></div>
+      <div class="dark-blue-layer"></div>
     </v-app>
   </div>
 </template>
@@ -263,9 +381,6 @@ export default {
         content: "6_EEYtY0wjahRpeFmJZ7qcVSuABHdi4mV5wa8Y1FmlE"
       }
     ]
-  },
-  created() {
-    console.log(this.$vuetify);
   }
 };
 </script>
@@ -274,5 +389,25 @@ export default {
 .fit-page {
   max-width: 100%;
   max-height: 100%;
+}
+.dark-blue-layer {
+  background: url("~@/vectors/dark_blue_layer.svg");
+  position: absolute;
+  height: 234px;
+  max-height: 100%;
+  width: 1180px;
+  max-width: 100%;
+  bottom: 0;
+  left: 0;
+}
+.light-blue-layer {
+  background: url("~@/vectors/light_blue_layer.svg");
+  position: absolute;
+  height: 388px;
+  max-height: 100%;
+  width: 1276px;
+  max-width: 100%;
+  bottom: 0;
+  left: 0;
 }
 </style>
