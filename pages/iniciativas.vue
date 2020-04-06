f88f32d3bf4efee6b30a299c50e543ba141c0674
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2020-04-06 15:07:21 -0300

    Create page iniciativas
    Just a place holder for a while

---
+++ pages/iniciativas.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Panel
+      title="Iniciativas"
+      description="A USP mantém diversas iniciativas e programas para facilitar e estimular a inovação e o empreendedorismo, fazendo a ponte entre o ambiente acadêmico, as organizações e a sociedade. Clique nos links para conhecer os tipos de inicativas e acessar as formas de contatar cada uma delas."
+    />
+
+    <v-container style="height: 20rem;">
+      <v-row class="fill-height" justify="center" align="center">
+        <div align="center" class="display-2 text-center">Página em construção</div>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import Panel from "../components/Panel.vue";
+export default {
+  components: {
+    Panel
+  }
+};
+</script>
