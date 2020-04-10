231757af4b7ad19709027e5fd92fff6ed7d37636
Gabriel Brandão <gabriel.brandaoal@gmail.com>
2020-04-10 18:41:08 -0300

    Create component for selection and info card

---
+++ components/ListAndCard.vue
@@ -0,0 +1,97 @@
+<template>
+  <v-container fluid>
+    <v-row justify="center" class="ma-0">
+      <v-col cols="5">
+        <v-card
+          height="35rem"
+          class="d-flex flex-column justicy-space-between"
+          :class="{'justify-center' : items.length == 0}"
+        >
+          <div v-if="items.length > 0">
+            <div class="d-flex justify-center">
+              <v-icon large>keyboard_arrow_up</v-icon>
+            </div>
+
+            <div style="height: 30rem">
+              <v-list rounded style="max-height: 100%; overflow-y: auto;">
+                <v-list-item-group v-model="current_item">
+                  <v-list-item v-for="item in items" :key="item.name">
+                    <slot name="li" :item="item">
+                      <v-list-item-content>
+                        <v-list-item-title>{{ item.name }}</v-list-item-title>
+                        <v-list-item-subtitle>{{item.key_words}}</v-list-item-subtitle>
+                      </v-list-item-content>
+                    </slot>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-icon large>keyboard_arrow_down</v-icon>
+            </div>
+          </div>
+          <div v-else>
+            <v-container>
+              <v-row class="fill-height ma-0" justify="center" align="center">
+                <p
+                  class="title font-weight-light text-center mb-0"
+                >Não encontramos nada relacionado a sua pesquisa</p>
+              </v-row>
+            </v-container>
+          </div>
+        </v-card>
+      </v-col>
+
+      <v-col cols="5">
+        <v-card height="35rem">
+          <div v-if="current_item >= 0" class="fill-height" style="overflow-y: auto;">
+            <slot name="item" :item="current_item">
+              <v-container px-6>
+                <p class="title">{{item.name}}</p>
+                <p class="body-2 font-italic my-2">{{item.knownledge.toString()}}</p>
+                <p class="body-2">{{item.campus}} - {{item.unity}}</p>
+              </v-container>
+
+              <v-container px-6>
+                <p class="body-2">{{item.description.long}}</p>
+              </v-container>
+
+              <v-card-actions>
+                <v-spacer />
+                <v-btn depressed dark color="rgb(255, 167, 38)" :href="item.url">Saiba mais</v-btn>
+                <v-spacer />
+              </v-card-actions>
+            </slot>
+          </div>
+
+          <div v-else class="fill-height">
+            <v-row class="fill-height ma-0" justify="center" align="center">
+              <p class="title font-weight-light text-center mb-0">Selecione um Item na lista ao lado</p>
+            </v-row>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    current_item: -1
+  }),
+  computed: {
+    item: function() {
+      if (this.current_item < 0) return null;
+      return this.items[this.current_item];
+    }
+  },
+  watch: {
+    items(v) {
+      this.current_item = -1;
+    }
+  },
+  props: ["items"]
+};
+</script>
---
+++ components/SelectAndCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row justify="center" class="ma-0">
+    <v-col cols="11" sm="10">
+      <v-card>
+        <v-container>
+          <v-select
+            flat
+            rounded
+            filled
+            hide-details
+            v-model="current_item"
+            menu-props="auto"
+            color="#37474F"
+            :items="items.map((item,i) => ({text: item.name, value: i}))"
+            no-data-text="Não encontramos nada"
+            label="Escolha uma disciplina"
+          >
+            <template v-slot:selection="{ item, index }">
+              <slot name="selection" :item="item" :index="index"></slot>
+            </template>
+          </v-select>
+        </v-container>
+
+        <div v-if="current_item >= 0">
+          <v-container px-6>
+            <p class="title">{{item.name}}</p>
+            <p class="body-2 font-italic my-2">{{item.category}}</p>
+            <p class="body-2">{{item.campus}} - {{item.unity}}</p>
+          </v-container>
+
+          <v-container px-6>
+            <p class="body-2">{{item.description.long}}</p>
+          </v-container>
+
+          <v-card-actions>
+            <v-spacer />
+            <v-btn depressed dark color="rgb(255, 167, 38)" :href="item.url">Saiba mais</v-btn>
+            <v-spacer />
+          </v-card-actions>
+        </div>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    current_item: -1
+  }),
+  computed: {
+    item: function() {
+      if (this.current_item < 0) return null;
+      return this.items[this.current_item];
+    }
+  },
+  watch: {
+    items(v) {
+      this.current_item = -1;
+    }
+  },
+  props: ["items"]
+};
+</script>
