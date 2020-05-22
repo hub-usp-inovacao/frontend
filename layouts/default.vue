ff9e6b2ea95b759c8bf78244fbb514cb720b1ca8
João Daniel <jotaf.daniel@gmail.com>
2020-05-22 16:40:47 -0300

    fix (nav|app)bar
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,38 @@
 <template>
   <v-app>
-    <v-toolbar class="hidden-sm-and-down" color="transparent" style="right: 0" absolute flat>
-      <v-btn
-        v-for="(item, i) in items"
-        :key="i"
-        :to="item.to"
-        class="white--text mx-2 secondary py-4 px-6 subtitle-1"
-        active-class="tertiary font-weight-bold"
-        rounded
-        small
-      >
-        <span class="text-capitalize">{{item.title}}</span>
-      </v-btn>
+    <v-toolbar
+      class="hidden-sm-and-down d-flex justify-end"
+      color="transparent"
+      width="75%"
+      style="margin-left: 25%;"
+      absolute
+      flat
+    >
+      <v-toolbar-items>
+        <v-chip-group>
+          <v-chip
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            class="white--text mx-2 secondary py-4 px-6 subtitle-1"
+          >{{ item.title }}</v-chip>
+        </v-chip-group>
+      </v-toolbar-items>
     </v-toolbar>
 
     <v-app-bar class="hidden-md-and-up" color="white" dense flat fixed>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group">
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.to">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-content :class="$vuetify.breakpoint.smAndDown ? 'mt-12' : ''">
       <v-snackbar :value="true" :timeout="0">
@@ -80,7 +96,9 @@ export default {
         title: "Contato",
         to: "/contato"
       }
-    ]
+    ],
+    drawer: false,
+    group: ""
   })
 };
 </script>
