b930870b136faee5f5213e506a364ca1849f28b7
João Daniel <jotaf.daniel@gmail.com>
2020-05-21 22:33:47 -0300

    add ListAndDetails new component
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/ListAndDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="5">
+        <List :items="items" v-model="selectedItem" />
+      </v-col>
+      <v-col cols="5">
+        <DetailsCard :item="selectedItem">
+          <template v-slot:content>
+            <slot name="content" v-bind:item="selectedItem"></slot>
+          </template>
+          <template v-slot:buttons>
+            <slot name="buttons" v-bind:item="selectedItem"></slot>
+          </template>
+        </DetailsCard>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import List from "./List.vue";
+import DetailsCard from "./DetailsCard.vue";
+
+export default {
+  components: {
+    List,
+    DetailsCard
+  },
+  props: ["items"],
+  data: () => ({
+    selectedItem: null
+  })
+};
+</script>
