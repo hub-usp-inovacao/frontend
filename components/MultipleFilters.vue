b7d74450a8f1c4f8c1ad3e3ae8e719a9d2dca9b0
João Daniel <jotaf.daniel@gmail.com>
2020-06-20 23:37:46 -0300

    WIP for MultipleFilters
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/MultipleFilters.vue
@@ -0,0 +1,96 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <CardButton :tabs="tabs" color="#aa5555" active="#ff9999" @select="selectedTabs = $event" />
+      </v-col>
+    </v-row>
+    <v-row v-if="needSubfilters">
+      <v-col>
+        <ChipSubfilter
+          :subareas="availableSubareas"
+          @subfilter-change="selectedSubfilters = $event"
+        />
+      </v-col>
+    </v-row>
+    <v-row v-if="needDropdown">
+      <v-col>
+        <DropdownFilter :groups="groups" @select="selectedDropdownFilters = $event" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import CardButton from "@/components/CardButton.vue";
+import ChipSubfilter from "@/components/ChipSubfilter.vue";
+import DropdownFilter from "@/components/DropdownFilter.vue";
+
+export default {
+  components: {
+    CardButton,
+    ChipSubfilter,
+    DropdownFilter
+  },
+  props: {
+    items: {
+      type: Array,
+      required: true
+    },
+    tabs: {
+      validator(givenTabs) {
+        return (
+          givenTabs instanceof Array &&
+          givenTabs.reduce((acc, tab) => {
+            const tabKeys = Object.keys(tab);
+
+            return (
+              acc &&
+              tabKeys.includes("name") &&
+              tabKeys.includes("subareas") &&
+              tab.subareas instanceof Array &&
+              tab.subareas.reduce(
+                (acc, sa) => acc && typeof sa === "string",
+                true
+              )
+            );
+          }, true)
+        );
+      }
+    },
+    groups: {
+      validator(givenGroups) {
+        return (
+          givenGroups instanceof Array &&
+          givenGroups.reduce(
+            (acc, { label, items }) =>
+              label &&
+              typeof label === "string" &&
+              items instanceof Array &&
+              items.every(item => typeof item === "string"),
+            true
+          )
+        );
+      }
+    }
+  },
+  data: () => ({
+    selectedTabs: [],
+    selectedSubfilters: [],
+    selectedDropdownFilters: []
+  }),
+  computed: {
+    availableSubareas() {
+      return this.tabs
+        .filter(({ name }) => this.selectedTabs.includes(name))
+        .reduce((acc, { subareas }) => acc.concat(subareas), []);
+    },
+    needSubfilters() {
+      return this.tabs.some(tab => tab.subareas !== undefined);
+    },
+    needDropdown() {
+      return this.groups !== undefined;
+    }
+  }
+};
+</script>
