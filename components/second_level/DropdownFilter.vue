ac2d1da7089db03b16e7affb8cf5c7c80c111912
João Daniel <jotaf.daniel@gmail.com>
2020-07-24 19:15:31 -0300

    add a friendly message for empty selects
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- components/second_level/DropdownFilter.vue
+++ components/second_level/DropdownFilter.vue
@@ -8,13 +8,25 @@
 
     <v-row class="hidden-sm-and-down">
       <v-col v-for="({ label, items }, i) of groups" :key="i" :cols="colSize">
-        <v-select outlined menu-props="auto" :label="label" :items="items" v-model="selected[i]"></v-select>
+        <v-select
+          :no-data-text="noData"
+          menu-props="auto"
+          :label="label"
+          :items="items"
+          v-model="selected[i]"
+        ></v-select>
       </v-col>
     </v-row>
 
     <v-row class="hidden-md-and-up" v-for="({ label, items }, i ) of groups" :key="i">
       <v-col cols="12">
-        <v-select outlined menu-props="auto" :label="label" :items="items" v-model="selected[i]"></v-select>
+        <v-select
+          :no-data-text="noData"
+          menu-props="auto"
+          :label="label"
+          :items="items"
+          v-model="selected[i]"
+        ></v-select>
       </v-col>
     </v-row>
   </v-container>
@@ -23,7 +35,8 @@
 <script>
 export default {
   data: () => ({
-    selected: []
+    selected: [],
+    noData: "em construção",
   }),
   props: {
     groups: {
@@ -36,20 +49,20 @@ export default {
             true
           )
         );
-      }
-    }
+      },
+    },
   },
   computed: {
     colSize() {
       const nGroups = this.groups.length;
 
       return nGroups > 4 ? Math.floor(12 / nGroups) : 3;
-    }
+    },
   },
   watch: {
     selected(list) {
       this.$emit("select", list);
-    }
-  }
+    },
+  },
 };
 </script>
