f02bb64d7d548d7d69ddc36ae5c23d85d82be5a8
Thiago Guerrero <tgbalera@hotmail.com>
2020-08-09 17:18:25 -0300

    Fix responsive searchDetailsCard display
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- components/second_level/SearchDetailsCard.vue
+++ components/second_level/SearchDetailsCard.vue
@@ -1,33 +1,14 @@
 <template>
   <div>
-    <v-card class="hidden-sm-and-down" height="35rem" style="overflow-y: auto;">
+    <v-card :style="cardStyle">
       <v-card-title>Resultados de busca para "{{ searchedTerm }}":</v-card-title>
       <v-card-text>
         <v-list v-if="filtered.length > 0">
           <v-list-item-group>
             <v-list-item three-line v-for="(item, index) in filtered" :key="index">
-              <v-list-item-content class="ml-2">
-                <v-list-item-title class="font-weight-bold">{{ item.name }}</v-list-item-title>
-                <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>
-              </v-list-item-content>
-              <v-list-item-action>
-                <v-chip>{{ item.category }}</v-chip>
-              </v-list-item-action>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
-        <p v-else>Nenhum resultado a exibir</p>
-      </v-card-text>
-    </v-card>
-    <v-card class="hidden-md-and-up">
-      <v-card-title>Resultados de busca para "{{ searchedTerm }}":</v-card-title>
-      <v-card-text>
-        <v-list v-if="filtered.length > 0">
-          <v-list-item-group>
-            <v-list-item three-line v-for="(item, index) in filtered" :key="index">
-              <v-list-item-content class="ml-2">
-                <v-list-item-title class="font-weight-bold">{{ item.name }}</v-list-item-title>
-                <v-list-item-subtitle>{{ item.description }}</v-list-item-subtitle>
+              <v-list-item-content :class="contentMarginStyle">
+                <v-list-item-title class="font-weight-bold item-title">{{ item.name }}</v-list-item-title>
+                <v-list-item-subtitle class="hidden-sm-and-down">{{ item.description }}</v-list-item-subtitle>
               </v-list-item-content>
               <v-list-item-action>
                 <v-chip>{{ item.category }}</v-chip>
@@ -43,9 +24,35 @@
 
 <script>
 export default {
-  props: ["searchedTerm", "filtered"]
+  props: ["searchedTerm", "filtered"],
+  computed: {
+    contentMarginStyle(){
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+        case 'sm':
+          return '';
+        default:
+          return 'ml-2';
+      }
+    },
+    cardStyle(){
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs':
+        case 'sm':
+          return;
+        default:
+          return {
+            overflowY: "auto",
+            height: "35rem"
+          }
+      }
+    }
+  }
 };
 </script>
 
 <style>
+.item-title {
+  white-space: normal;
+}
 </style>
