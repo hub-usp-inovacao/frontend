39dec5696dfe21819486902ae296b4e162375135
Thiago Guerrero <tgbalera@hotmail.com>
2020-08-14 13:07:59 -0300

    Add line number display at ErrorsTable
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- components/temporary/ErrorsTable.vue
+++ components/temporary/ErrorsTable.vue
@@ -1,14 +1,16 @@
 <template>
   <v-container class="mt-10" width="90%">
     <v-row>
+      <v-col cols="1">Linha</v-col>
       <v-col cols="3">Nome</v-col>
-      <v-col cols="9">Possíveis problemas</v-col>
+      <v-col cols="8">Possíveis problemas</v-col>
     </v-row>
     <div class="overflow">
       <v-row
         v-for="({ id, line, errors }, i) in errors"
         :key="id"
         :style="rowStyle(i)">
+        <v-col cols="1">{{line}}</v-col>
         <v-col cols="3">{{ id }}</v-col>
         <v-col cols="3">
           <ul>
