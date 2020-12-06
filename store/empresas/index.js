9498cd02fd1d242ed1a222dc55936fcfdf8ae8d9
João Daniel <jotaf.daniel@gmail.com>
2020-12-05 22:47:18 -0300

    update empresas
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- store/empresas/index.js
+++ store/empresas/index.js
@@ -37,7 +37,7 @@ export const mutations = {
   setLoadingStatus: (s) => (s.isLoading = true),
   unsetLoadingStatus: (s) => (s.isLoading = false),
   setCompanies: (s, newCompanies) => (s.companies = newCompanies),
-  setErrors: (s, errors) => s.errors = errors,
+  setErrors: (s, errors) => (s.errors = errors),
 };
 
 export const actions = {
@@ -55,7 +55,17 @@ export const actions = {
 
       const data = await resp.json();
 
-      const objects = data.values.slice(1).map((row) => CompanyGenerator.run(row));
+      const objects = data.values.slice(1).map((row, i) => {
+        let company;
+        try {
+          company = CompanyGenerator.run(row);
+        } catch (e) {
+          console.log(`[Company Exception] failed for row ${i + 2}`);
+          company = null;
+        }
+
+        return company;
+      });
 
       const errors = findErrors(Object.assign([], objects));
 
@@ -64,7 +74,7 @@ export const actions = {
       ctx.commit(
         "setCompanies",
         objects
-          .filter((c) => c.allowed && c.active)
+          .filter((c) => c !== null && c.allowed && c.active)
           .sort((a, b) => a.name.localeCompare(b.name))
       );
     } catch (error) {
