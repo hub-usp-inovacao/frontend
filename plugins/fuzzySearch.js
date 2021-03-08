3aabd6a543a4d7ac28635e297f43c7095c4ca4a1
João Daniel <jotaf.daniel@gmail.com>
2021-03-08 18:37:29 -0300

    create real fuzzy plugin
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- plugins/fuzzySearch.js
+++ plugins/fuzzySearch.js
@@ -1,4 +1,30 @@
-export default (first, inject) => {
-  console.log(Object.getPrototypeOf(first));
-  inject("fuzzySearch", () => console.log("oi"));
+const generateOption = (keys, threshold = 0) => ({
+  ignoreLocation: true,
+  findAllMatches: true,
+  shouldSort: true,
+  tokenize: true,
+  matchAllTokens: true,
+  maxPatternLength: 32,
+  minMatchCharLength: 2,
+  threshold,
+  keys,
+});
+
+export default (context, inject) => {
+  inject("fuzzySearch", async (term, items, keys) => {
+    context.store.dispatch("global/setStrictResults");
+
+    if (!term) {
+      return undefined;
+    }
+
+    let results = await context.$search(term, items, generateOption(keys));
+
+    if (!results.length) {
+      context.store.dispatch("global/setFlexibleResults");
+      results = await context.$search(term, items, generateOption(keys, 0.15));
+    }
+
+    return results;
+  });
 };
