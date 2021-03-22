5e779d5564f9b1cdb32fa80e65c1d0d5a1a3b2b7
João Daniel <jotaf.daniel@gmail.com>
2021-03-22 10:43:28 -0300

    improve implementations in patents generator
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- plugins/services/fetch_patents.js
+++ plugins/services/fetch_patents.js
@@ -20,29 +20,50 @@ async function fetchData(sheetsAPIKey) {
   }
 }
 
-function PatentGenerator(row) {
-  const base = new Patent(
-    columnValue(row, "F"),
-    columnValue(row, "K"),
-    {
-      primary: {
-        cip: columnValue(row, "A").trim(),
-        subarea: columnValue(row, "B").trim(),
-      },
-      secondary: {
-        cip: columnValue(row, "C").trim(),
-        subarea: columnValue(row, "D").trim(),
-      },
+function beginNewPatent(row) {
+  const name = columnValue(row, "F");
+  const summary = columnValue(row, "K");
+  const classification = {
+    primary: {
+      cip: columnValue(row, "A").trim(),
+      subarea: columnValue(row, "B").trim(),
     },
-    columnValue(row, "G").split(" | "),
-    columnValue(row, "I").split(" | "),
-    columnValue(row, "M")
-  );
+    secondary: {
+      cip: columnValue(row, "C").trim(),
+      subarea: columnValue(row, "D").trim(),
+    },
+  };
+  const ipcs = columnValue(row, "G").split(" | ");
+  const owners = columnValue(row, "I").split(" | ");
+  const status = columnValue(row, "M");
 
+  return new Patent(name, summary, classification, ipcs, owners, status);
+}
+
+function addURL(base, row) {
   base.url = columnValue(row, "O");
+}
+
+function addInventors(base, row) {
   base.inventors = columnValue(row, "J");
+}
+
+function addCountries(base, row) {
   base.countriesWithProtection = columnValue(row, "L");
+}
+
+function addPhoto(base, row) {
   base.photo = columnValue(row, "P");
+}
+
+function PatentGenerator(row) {
+  const base = beginNewPatent(row);
+
+  addURL(base, row);
+  addInventors(base, row);
+  addCountries(base, row);
+  addPhoto(base, row);
+
   return base;
 }
 
