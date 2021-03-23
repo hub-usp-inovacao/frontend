ee8e721b5f8a9b9f90fe7e982ca62cb92bdf4843
João Daniel <jotaf.daniel@gmail.com>
2021-03-23 09:12:43 -0300

    fix dash in incubator's names
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- plugins/company.js
+++ plugins/company.js
@@ -1,16 +1,24 @@
+const noIncub = "Direto para o Mercado";
+
+const sortWithNoIncubFirst = (a, b) => {
+  if (a === noIncub) return -1;
+  if (b === noIncub) return 1;
+  return a.localeCompare(b);
+};
+
 const companyConsts = {
   incubators: [
-    "Direto para o Mercado",
-    "CIETEC – Centro de Inovação, Empreendedorismo e Tecnologia",
-    "ESALQTec – Incubadora de Empresas Agrozootécnicas de Piracicaba",
-    "HABITs – Habitat de Inovação Tecnológica e Social/Incubadora-Escola",
-    "INOVA-HC",
+    noIncub,
+    "CIETEC - Centro de Inovação, Empreendedorismo e Tecnologia",
+    "ESALQTec - Incubadora de Empresas Agrozootécnicas de Piracicaba",
+    "HABITs - Habitat de Inovação Tecnológica e Social/Incubadora-Escola",
+    "Inova HC – Núcleo de Inovação do Hospital das Clínicas",
     "InovaLab@POLI",
-    "ParqTec – Fundação Parque Tecnológico de São Carlos",
+    "ParqTec - Fundação Parque Tecnológico de São Carlos",
     "Parque Tecnológico Univap",
-    "Pqtec – Parque Tecnológico São José dos Campos",
-    "Supera – Incubadora de Empresas de Base Tecnológica de Ribeirão Preto",
-  ],
+    "Pqtec - Parque Tecnológico São José dos Campos",
+    "Supera - Incubadora de Empresas de Base Tecnológica de Ribeirão Preto",
+  ].sort(sortWithNoIncubFirst),
   sizes: ["Microempresa", "Pequena Empresa", "Média Empresa", "Grande Empresa"],
 };
 
