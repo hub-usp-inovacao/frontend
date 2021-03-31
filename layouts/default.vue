9f6901727cdcdb98015d24f61b92357e45679dd0
Thiago Guerrero <tgbalera@hotmail.com>
2021-03-31 17:24:54 -0300

    remove migration alert
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -37,16 +37,13 @@
 
     <v-main :class="$vuetify.breakpoint.smAndDown ? 'mt-12' : ''">
       <v-snackbar :multi-line="true" :value="true" :timeout="-1">
-        Como está sendo sua experiência com o Hub?
+        Como está sendo sua experiência com o Hub USP Inovação?
         <v-btn
           class="ml-2"
           href="https://forms.gle/TiEe6zYq9GsYgDww5"
           target="_blank"
           >Relate aqui</v-btn
         >
-        <p class="mt-4 font-weight-bold caption text-center">
-          Em breve: migração para o domínio usp.br
-        </p>
       </v-snackbar>
       <nuxt />
     </v-main>
