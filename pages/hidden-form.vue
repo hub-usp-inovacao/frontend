c0fa37bbab82cea46b2fb50cd580d770e3a42aa9
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-05-07 16:46:01 -0300

    add hidden page to redirect to company form
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ pages/hidden-form.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Secret page</h1>
+</template>
+
+<script>
+export default {
+  middleware({ redirect }) {
+    return redirect(
+      'https://docs.google.com/forms/d/1jgRwsh8kXhMrGoxlRxKIK-ZcUwuVER62zN2egBRLa9c'
+    );
+  },
+};
+</script>
