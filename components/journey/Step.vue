79ccb99a3926d44454e4178af0b211a8d496d96d
João Daniel <jotaf.daniel@gmail.com>
2021-06-29 16:02:22 -0300

    define layout for each step
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

--- components/journey/Step.vue
+++ components/journey/Step.vue
@@ -1,39 +1,80 @@
 <template>
-  <main>
+  <main class="bg-gray">
     <v-container class="hidden-sm-and-down">
       <v-row>
         <v-col>
-          {{ title }}
+          <v-row>
+            <v-col cols="6">
+              <div
+                style="margin-left: -10vw"
+                class="py-4 red text-center white--text font-weight-bold text-h2 rounded-lg"
+              >
+                {{ title }}
+              </div>
+            </v-col>
+            <v-col cols="2">
+              <div
+                style="margin-left: 15vw; height: 100%"
+                class="py-4 rounded-lg orange"
+              ></div>
+            </v-col>
+          </v-row>
         </v-col>
       </v-row>
       <v-row>
-        <v-col>
+        <v-col cols="4">
           <v-row>
             <v-col>{{ description }}</v-col>
           </v-row>
           <v-row>
-            <v-col>{{ previous }} {{ next }}</v-col>
+            <v-col>
+              <JourneyNav :next="nextLink" :previous="previousLink" />
+            </v-col>
           </v-row>
         </v-col>
-        <v-col>
+        <v-col offset="1" cols="7">
           <slot></slot>
         </v-col>
       </v-row>
     </v-container>
 
     <v-container class="hidden-md-and-up">
-      <div class="d-flex flex-column">
-        <section>{{ title }}</section>
-        <section>{{ description }}</section>
-        <section><slot></slot></section>
-        <section>{{ previous }} {{ next }}</section>
-      </div>
+      <v-row>
+        <v-col cols="12">
+          <div
+            style="margin-left: -10vw"
+            class="py-4 red text-center white--text font-weight-bold text-h2 rounded-lg"
+          >
+            {{ title }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          {{ description }}
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <slot></slot>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <JourneyNav :next="nextLink" :previous="previousLink" />
+        </v-col>
+      </v-row>
     </v-container>
   </main>
 </template>
 
 <script>
+import JourneyNav from "@/components/journey/JourneyNav.vue";
+
 export default {
+  components: {
+    JourneyNav,
+  },
   props: {
     title: {
       type: String,
@@ -52,5 +93,29 @@ export default {
       required: true,
     },
   },
+
+  computed: {
+    nextLink() {
+      if (this.next) {
+        return { label: "Avançar", to: this.next };
+      }
+
+      return { label: "Voltar ao menu", to: "/jornada" };
+    },
+
+    previousLink() {
+      if (this.previous) {
+        return { label: "Voltar", to: this.previous };
+      }
+
+      return { label: "Voltar ao menu", to: "/jornada" };
+    },
+  },
 };
 </script>
+
+<style scoped>
+.bg-gray {
+  background-color: #ececec;
+}
+</style>
