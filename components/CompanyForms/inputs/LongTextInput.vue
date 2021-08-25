4242248ba1e0b5c07e1f111f7029dd6b67e592f1
João Daniel <jotaf.daniel@gmail.com>
2021-08-25 10:33:58 -0300

    add LongTextInput
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/CompanyForms/inputs/LongTextInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-textarea
+    :value="value"
+    @input="$emit('input', $event)"
+    :clearable="clearable"
+    rows="2"
+    :label="label"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    clearable: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
