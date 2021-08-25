876972f38e54030952e449411253de1bc1baa0cc
João Daniel <jotaf.daniel@gmail.com>
2021-08-25 10:25:46 -0300

    add ShortTextInput
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/CompanyForms/inputs/ShortTextInput.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-text-field
+    :value="value"
+    @input="$emit('input', $event)"
+    :clearable="clearable"
+    :counter="counter"
+    :label="label"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    clearable: {
+      type: Boolean,
+      required: false,
+      default: true,
+    },
+    maxLength: {
+      type: Number,
+      required: false,
+      default: -1
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    counter() {
+      return this.maxLength <= 0 ? false : this.maxLength;
+    },
+  },
+}
+</script>
