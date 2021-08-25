e1b0999c3c38a86366cc0983cea3284e828d0963
João Daniel <jotaf.daniel@gmail.com>
2021-08-25 14:31:23 -0300

    add Dropdown
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/CompanyForms/inputs/Dropdown.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-select
+    :items="options"
+    :value="value"
+    :label="label"
+    @input="$emit('input', $event)"
+    clearable
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Array,
+      required: true,
+    },
+    value: {
+      required: true,
+    },
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
