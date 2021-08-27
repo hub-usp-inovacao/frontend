b64eca3a9a063f0507f0e20c8f637364bdbb0ee4
Luiz Carlos <luizcarlosdk@usp.br>
2021-08-27 00:00:12 -0300

    Add Boolean Input
    Signed-off-by: Luiz Carlos <luizcarlosdk@usp.br>

---
+++ components/CompanyForms/inputs/BooleanInput.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="radio" :value="valueTrue" input="$emit('input', $event)" />
+    <input type="radio" :value="valueFalse" input="$emit('input', $event)" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    valueTrue: {
+      type: String,
+      required: true,
+    },
+    valueFalse: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
