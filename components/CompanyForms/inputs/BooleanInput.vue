8b3ab43f05fdd2f1c3f4deb83693541a253e78f3
Luiz Carlos <luizcarlosdk@usp.br>
2021-08-27 15:45:47 -0300

    Remove radio input from BooleanInput
    Signed-off-by: Luiz Carlos <luizcarlosdk@usp.br>

--- components/CompanyForms/inputs/BooleanInput.vue
+++ components/CompanyForms/inputs/BooleanInput.vue
@@ -1,47 +1,26 @@
 <template>
-  <div>
-    <v-radio-group v-if="valueFalse != undefined" v-model="booleanRadio" column>
-      <v-radio
-        :label="valueTrue"
-        :color="color"
-        :value="value"
-        input="$emit('input', $event)"
-      >
-      </v-radio>
-      <v-radio
-        :label="valueFalse"
-        :color="color"
-        :value="value"
-        input="$emit('input', $event)"
-      >
-      </v-radio>
-    </v-radio-group>
-    <v-switch
-      v-else
-      :v-model="booleanSwitch"
-      :label="valueTrue"
-      :color="color"
-      :value="value"
-      input="$emit('input', $event)"
-    >
-    </v-switch>
-  </div>
+  <v-switch
+    :v-model="booleanSwitch"
+    :label="label"
+    :color="color"
+    :value="value"
+    input="$emit('input', $event)"
+  >
+  </v-switch>
 </template>
 
 <script>
 export default {
   props: {
     value: {
-      type: String,
+      type: Boolean,
+      default: false,
     },
-    valueTrue: {
+    label: {
       type: String,
       required: true,
     },
-    valueFalse: {
-      type: String,
-      default: undefined,
-    },
+
     color: {
       type: String,
       default: "blue",
@@ -49,7 +28,6 @@ export default {
   },
 
   data: () => ({
-    booleanRadio: null,
     booleanSwitch: false,
   }),
 };
