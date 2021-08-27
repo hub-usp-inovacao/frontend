2d351c14924b6ea06a08761acd7f574f0a4159a4
João Daniel <jotaf.daniel@gmail.com>
2021-08-27 16:58:05 -0300

    dummy: create about component with short description
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ components/CompanyForms/companyStep/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-form>
+    <LongTextInput label="Breve Descrição" v-model="description" clearable />
+  </v-form>
+</template>
+
+<script>
+import ShortTextInput from "@/components/CompanyForms/inputs/ShortTextInput.vue";
+import LongTextInput from "@/components/CompanyForms/inputs/LongTextInput.vue";
+import ImageUploader from "@/components/CompanyForms/inputs/ImageUploader.vue";
+import URLInput from "@/components/CompanyForms/inputs/URLInput.vue";
+
+export default {
+  components: {
+    ShortTextInput,
+    LongTextInput,
+    ImageUploader, 
+    URLInput,
+  },
+  data: () => ({
+    description: '',
+  }),
+};
+</script>
---
+++ pages/empresas/cadastrar.vue
@@ -0,0 +1,13 @@
+<template>
+  <About />
+</template>
+
+<script>
+import About from "@/components/CompanyForms/companyStep/About.vue";
+
+export default {
+  components: {
+    About,
+  },
+};
+</script>
