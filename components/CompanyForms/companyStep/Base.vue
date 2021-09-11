0661f8a59deafbdfcc93f2942c2b6cb68d752318
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-09-11 14:47:09 -0300

    fix cnae dropdowns and add v-models
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

--- components/CompanyForms/companyStep/Base.vue
+++ components/CompanyForms/companyStep/Base.vue
@@ -1,25 +1,35 @@
 <template>
   <v-container>
-    <ShortTextInput label="Nome" />
-    <ShortTextInput label="Razão social" />
-    <ShortTextInput label="Ano" />
-    <ShortTextInput label="CNPJ" />
-    <h2>CNAE</h2>
-    <v-row>
-      <v-col cols="6">
-        <Dropdown label="Área principal" :options="cnaeMajors" />
-      </v-col>
-      <v-col cols="6">
-        <ShortTextInput label="Área secundária" />
-      </v-col>
-    </v-row>
-    <ShortTextInput label="Telefone" />
-    <ShortTextInput label="Email" />
-    <ShortTextInput label="Endereço" />
-    <ShortTextInput label="Bairro" />
-    <ShortTextInput label="Cidade" />
-    <Dropdown label="Estado" :options="states" />
-    <ShortTextInput label="CEP" />
+    <v-form>
+      <ShortTextInput v-model="name" label="Nome" />
+      <ShortTextInput v-model="corporateName" label="Razão social" />
+      <ShortTextInput v-model="year" label="Ano" />
+      <ShortTextInput v-model="cnpj" label="CNPJ" />
+      <h2>CNAE</h2>
+      <v-row>
+        <v-col cols="6">
+          <Dropdown
+            v-model="cnae.major"
+            label="Área principal"
+            :options="cnaeMajors"
+          />
+        </v-col>
+        <v-col cols="6">
+          <Dropdown
+            v-model="cnae.minor"
+            label="Área secundária"
+            :options="cnaeMinors(cnae.major)"
+          />
+        </v-col>
+      </v-row>
+      <ShortTextInput v-model="phone" label="Telefone" />
+      <ShortTextInput v-model="email" label="Email" />
+      <ShortTextInput v-model="address" label="Endereço" />
+      <ShortTextInput v-model="neighborhood" label="Bairro" />
+      <ShortTextInput v-model="city" label="Cidade" />
+      <Dropdown v-model="state" label="Estado" :options="allStates" />
+      <ShortTextInput v-model="cep" label="CEP" />
+    </v-form>
   </v-container>
 </template>
 
@@ -33,24 +43,44 @@ export default {
     Dropdown,
   },
   data: () => ({
-    states: [],
+    name: "",
+    corporateName: "",
+    year: "",
+    cnpj: "",
+    cnae: {
+      major: "",
+      minor: "",
+    },
+    phone: "",
+    email: "",
+    address: "",
+    neighborhood: "",
+    city: "",
+    state: "",
+    cep: "",
+
+    allStates: [],
   }),
   computed: {
     cnaeMajors() {
       return Object.keys(this.$reverseCNAE).sort();
     },
   },
-  async beforeMount() {
-    const response = await fetch(
-      "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
-    );
-    const ufs = await response.json();
-    this.states = ufs.map((uf) => uf.nome).sort();
+  beforeMount() {
+    this.getStates();
   },
   methods: {
+    async getStates() {
+      const response = await fetch(
+        "https://servicodados.ibge.gov.br/api/v1/localidades/estados"
+      );
+      const ufs = await response.json();
+      this.allStates = ufs
+        .map(({ nome, sigla }) => `${nome} - ${sigla}`)
+        .sort();
+    },
     cnaeMinors(major) {
-      if (!major) return undefined;
-      return this.$reverseCNAE[major].map(({ minor }) => minor);
+      return major ? this.$reverseCNAE[major].map(({ minor }) => minor) : [];
     },
   },
 };
