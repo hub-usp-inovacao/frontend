d79fb57c0319a6c4c1bbedfa3c3762559637e3ca
Luiz Carlos <luizcarlosdk@usp.br>
2021-09-17 18:07:26 -0300

    Rename number input to mask input
    Signed-off-by: Luiz Carlos <luizcarlosdk@usp.br>

---
+++ components/CompanyForms/inputs/MaskNumber.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-text-field
+      v-mask="mask"
+      :label="label"
+      type="text"
+      :rules="rules.input"
+      :value="value"
+      @input="$emit('input', $event)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    rule: {
+      type: RegExp,
+      required: false,
+      default: () => /./,
+    },
+    mask: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  computed: {
+    rules() {
+      return {
+        input: [(f) => this.rule.test(f) || "Formato inválido"],
+      };
+    },
+  },
+};
+</script>
