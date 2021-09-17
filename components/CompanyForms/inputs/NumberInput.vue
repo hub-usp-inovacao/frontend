8619674fb0bfe7f49ccc7a55352ca33f18f25a9a
Luiz Carlos <luizcarlosdk@usp.br>
2021-09-17 17:28:52 -0300

    Add Number input
    Signed-off-by: Luiz Carlos <luizcarlosdk@usp.br>
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ components/CompanyForms/inputs/NumberInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <v-text-field v-mask="mask" type="text" :rules="rules.input" @input="$emit('input',$event)" :value="value"/>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    rule: {
+      type: RegExp,
+      required: false,
+      default: () => /./
+    },
+    mask: {
+      type: String,
+      default: ""
+    },
+    value: {
+      type: String
+    }
+  },
+
+  computed: {
+    rules() {
+      return {
+        input: [(f) => this.rule.test(f) || "Formato inválido"],
+      }
+    }
+  },
+};
+</script>
