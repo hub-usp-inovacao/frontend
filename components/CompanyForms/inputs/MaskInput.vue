21949eb2bcc58da1b5725cc0d714e279896bc32f
Luiz Carlos <luizcarlosdk@usp.br>
2021-09-17 18:14:38 -0300

    Change component name and add last updated
    Signed-off-by: Luiz Carlos <luizcarlosdk@usp.br>

---
+++ components/CompanyForms/inputs/MaskInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <LastUpdated :label="label" :last-updated="lastUpdated" />
+    <v-text-field
+      v-mask="mask"
+      :label="label"
+      type="text"
+      :rules="rules.input"
+      :value="value"
+      @input="$emit('input', $event)"
+    />
+  </div>
+</template>
+
+<script>
+import LastUpdated from "@/components/CompanyForms/inputs/LastUpdated.vue";
+
+export default {
+  components: {
+    LastUpdated,
+  },
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    rule: {
+      type: RegExp,
+      required: false,
+      default: () => /./,
+    },
+    mask: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+    },
+  },
+
+  computed: {
+    rules() {
+      return {
+        input: [(f) => this.rule.test(f) || "Formato inválido"],
+      };
+    },
+  },
+};
+</script>
