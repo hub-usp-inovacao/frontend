362e48d420b3d59cfe5de5dbe1492bd99c4a5cb1
Erick Rodrigues <erick.rodrigues.santana14@gmail.com>
2021-09-24 17:36:44 -0300

    add company form tabs
    Signed-off-by: Erick Rodrigues <erick.rodrigues.santana14@gmail.com>

---
+++ components/CompanyForms/Tabs.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container class="tab-container">
+    <v-tabs v-model="tab" fixed-tabs>
+      <v-tab v-for="item in items" :key="item.tab">
+        {{ item.tab }}
+      </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab">
+      <v-tab-item v-for="item in items" :key="item.tab">
+        <component :is="item.content"></component>
+      </v-tab-item>
+    </v-tabs-items>
+  </v-container>
+</template>
+
+<script>
+import Base from "@/components/CompanyForms/companyStep/Base.vue";
+import About from "@/components/CompanyForms/companyStep/About.vue";
+
+export default {
+  components: {
+    Base,
+    About,
+  },
+  data: () => ({
+    tab: null,
+    items: [
+      { tab: "Base", content: Base },
+      { tab: "Sobre", content: About },
+    ],
+  }),
+};
+</script>
+
+<style>
+.tab-container {
+  border: 1px solid rgba(0, 0, 0, 0.4);
+  padding: 0;
+}
+</style>
